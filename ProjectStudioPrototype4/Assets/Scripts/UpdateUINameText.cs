c424a2a0187f9cdca1d28c00f264fedaef9bcbd2
DESKTOP-AV91D2I\paosa <paosalcedo@gmail.com>
2017-10-23 13:35:23 -0400

    Name input works; updates the switch messages

--- ProjectStudioPrototype4/Assets/Scripts/UpdateUINameText.cs
+++ ProjectStudioPrototype4/Assets/Scripts/UpdateUINameText.cs
@@ -12,6 +12,12 @@ public class UpdateUINameText : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		nameText.text = "Your turn is over! Switching to " + PlayerNames.playerOneName;
+		if(CurrentPlayerTracker.currentPlayer.GetComponent<StealthPlayerSwitcher>().myIndex == 0){
+			nameText.text = "Your turn is over! Switching to " + PlayerNames.playerOneName;
+		}
+
+		if(CurrentPlayerTracker.currentPlayer.GetComponent<StealthPlayerSwitcher>().myIndex == 1){
+			nameText.text = "Your turn is over! Switching to " + PlayerNames.playerTwoName;
+		}
 	}
 }
