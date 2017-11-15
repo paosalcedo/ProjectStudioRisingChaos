33360904a34764fef0dda1074fda5fba69fa69ef
DESKTOP-AV91D2I\paosa <paosalcedo@gmail.com>
2017-11-14 19:52:55 -0500

    Reflectoid now uses Rigidbody instead of raycast. Thanks Kevin.

--- ProjectStudioPrototype4/Assets/Scripts/ReflectoidEngine.cs
+++ ProjectStudioPrototype4/Assets/Scripts/ReflectoidEngine.cs
@@ -26,46 +26,70 @@ public class ReflectoidEngine : MonoBehaviour {
 		initialDir = direction;
 		Debug.Log("Direction taken!");
 	}
-	private bool isInFirstTween;
-	private bool isInSecondTween;
-	private bool isInThirdTween;
+	public bool isInFirstTween;
+	public bool isInSecondTween;
+	public bool isInThirdTween;
+	public Vector3 firstReflection;
+	public Vector3 secondReflection;
+	private Vector3 firstRayDir;
+	private Vector3 secondRayDir;
+	private Vector3 thirdRayDir;
+
+	private Rigidbody rb;
 
  	public void Start () {
+		rb = GetComponent<Rigidbody>();
 		// parent = transform.parent.gameObject;
 		isInFirstTween = false;
 		isInSecondTween = false;
 		isInThirdTween = false;
 		reflectionState = ReflectionState.None;
+		rb.AddForce(transform.forward * 25f, ForceMode.Impulse);
 	}
 	
 	
 	// Update is called once per frame
 	void Update () {
-		if(firstPoint != Vector3.zero){
-			if(!isInFirstTween && !isInSecondTween && !isInThirdTween){
-				transform.DOMove(firstPoint, 0.5f, false);
-				if(Vector3.Distance(transform.position, firstPoint) <= 0.1f){
- 					isInFirstTween = true;
-				}
-			} 
 
-			if (isInFirstTween && !isInSecondTween && !isInThirdTween){
-				Debug.Log("Starting second path!");
-				transform.DOMove(secondPoint, 0.5f, false);
-				if(Vector3.Distance(transform.position, secondPoint) <= 0.1f){
-					isInSecondTween = true;
-				}
-			}		
+		// rb.AddForce()
+		// if(!isInFirstTween && !isInSecondTween && !isInThirdTween){
+		// 	if(firstPoint != Vector3.zero){
+		// 		transform.DOMove(firstPoint, 0.5f, false);
+		// 		if(Vector3.Distance(transform.position, firstPoint) <= 0.1f){
+ 		// 			isInFirstTween = true;
+		// 		}
+		// 	} else if (firstPoint == Vector3.zero){
+		// 		Debug.Log("First reflection end!");
+		// 		transform.Translate(firstReflection * 10f * Time.deltaTime);
+		// 	} 
+		// }  
+		
+		// if (isInFirstTween && !isInSecondTween && !isInThirdTween){
+		// 	if(secondPoint != Vector3.zero){
+		// 		Debug.Log("Starting second path!");
+		// 		transform.DOMove(secondPoint, 0.5f, false);
+		// 		if(Vector3.Distance(transform.position, secondPoint) <= 0.1f){
+		// 			isInSecondTween = true;
+		// 		}
+		// 	}
+		// 	else if (secondPoint == Vector3.zero){
+		// 		Debug.Log("Second reflection end!");
+		// 		transform.Translate(firstReflection * 10f * Time.deltaTime);
+		// 		// Destroy(gameObject);
+		// 	}
+		// } 
 
-			if (isInFirstTween && isInSecondTween && !isInThirdTween){
-				Debug.Log("Starting third path!");
-				transform.DOMove(thirdPoint, 0.5f, false);
-				if(Vector3.Distance(transform.position, thirdPoint) <= 0.1f){
-					isInThirdTween = true;
-					Destroy(gameObject, 1f);
-				}
-			}
-		}
+		// if (isInFirstTween && isInSecondTween && !isInThirdTween){
+		// 	if(thirdPoint != Vector3.zero){
+		// 		Debug.Log("Starting third path!");
+		// 		transform.DOMove(thirdPoint, 0.5f, false);
+		// 		if(Vector3.Distance(transform.position, thirdPoint) <= 0.1f){
+		// 			isInThirdTween = true;
+		// 			Destroy(gameObject, 1f);
+		// 		}
+		// 	}
+		// }
+
 		// switch(reflectionState){
 		// 	case ReflectionState.None:			
 		// 	if(!isTweening){
@@ -92,7 +116,7 @@ public class ReflectoidEngine : MonoBehaviour {
 
 		Debug.DrawRay(transform.position, transform.forward * 100f, Color.red, 3f);
  		if(Physics.Raycast(ray, out rayHit, Mathf.Infinity)){
-			Vector3 firstReflection = Vector3.Reflect(ray.direction, rayHit.normal);
+			firstReflection = Vector3.Reflect(ray.direction, rayHit.normal);
 			reflectionPoints.Add(rayHit.point);
 			Debug.DrawRay (rayHit.point, firstReflection * Mathf.Infinity, Color.blue);
 			if(rayHit.transform.tag == "Player"){
@@ -104,13 +128,14 @@ public class ReflectoidEngine : MonoBehaviour {
 				reflectionState = ReflectionState.One;					 
  				Ray secondRay = new Ray(rayHit.point, firstReflection);
 				RaycastHit secondRayHit = new RaycastHit();
-				Debug.DrawRay(rayHit.point, firstReflection * 100f, Color.blue, 3f);			
+				Debug.DrawRay(rayHit.point, firstReflection * 100f, Color.blue, 3f);		
+				firstRayDir = firstReflection;
 				// reflectoid.transform.DOMove(secondRayHit.point, 0.5f, false);
 				if(Physics.Raycast(secondRay, out secondRayHit, Mathf.Infinity)){
 					if(secondRayHit.transform.tag != "Player"){
 						secondPoint = secondRayHit.point;
 						reflectionPoints.Add(secondRayHit.point);
-						Vector3 secondReflection = Vector3.Reflect(secondRay.direction, secondRayHit.normal);
+						secondReflection = Vector3.Reflect(secondRay.direction, secondRayHit.normal);
 						Ray thirdRay = new Ray(secondRayHit.point, secondReflection);
 						RaycastHit thirdRayHit = new RaycastHit();	
 						Debug.DrawRay(secondRayHit.point, secondReflection * 100f, Color.green, 3f);
@@ -125,16 +150,9 @@ public class ReflectoidEngine : MonoBehaviour {
 						}
 					} 
 					//if reflection hit the player
-					else {
-
-					}
 				} 
-
 			}
-			 else {
-				Debug.Log("Hit nothing!");
-			}	
-		}
+		} 
 	}
 
 	void OnTriggerEnter(Collider coll){
